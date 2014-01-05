20b389c579e93941e5333f3463fe92cd09cf47ef
Roman Tsukanov <roman@tsukanov.me>
2014-01-05 14:52:42 +1000

    Created a project.
    Added basic objects and scripts.

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BallMovement :MonoBehaviour {
+
+		void Start() {
+				gameObject.rigidbody2D.AddForce(Vector2.right);
+		}
+
+		void Update() {
+				// TODO: Implement
+		}
+
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+		public float MoveForce = 40f;
+		public float MaxSpeed = 2f;
+		public string AxisName;
+
+		void Update() {
+				float v = Input.GetAxis(AxisName);
+				if (v * rigidbody2D.velocity.y < MaxSpeed) {
+						rigidbody2D.AddForce(Vector2.up * v * MoveForce);
+				}
+		}
+}
