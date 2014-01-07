3d566c8e9cd33d493c2c8acac670b07202483adf
Roman Tsukanov <roman@tsukanov.me>
2014-01-07 22:34:50 +1000

    Fixed issue that caused ball to get stuck in borders or players. Closes #7.

--- Assets/BallMovement.cs
+++ Assets/BallMovement.cs
@@ -19,33 +19,42 @@ public class BallMovement :MonoBehaviour
 		public float BorderLeft = -10;
 		public float BorderRight = 10;
 
+		// Safe direction
+		private bool goingDown;
+		private bool goingRight;
+
 		void Start ()
 		{
+				// Ball starts by going to the top right side
 				Direction.x = 6;
 				Direction.y = 8;
+				goingDown = false;
+				goingRight = true;
 		}
 
 		void Update ()
 		{		
 				// Win detection
-				if (transform.position.x < BorderLeft) { // Right wins			
+				if (transform.position.x < BorderLeft && !goingRight) { // Right wins			
 						Reset ();
 						ScoreRight.guiText.text = (int.Parse (ScoreRight.guiText.text) + 1).ToString ();
-				} else if (transform.position.x > BorderRight) { // Left wins
+				} else if (transform.position.x > BorderRight && goingRight) { // Left wins
 						Reset ();
 						ScoreLeft.guiText.text = (int.Parse (ScoreLeft.guiText.text) + 1).ToString ();
 				} 
 
-				if (transform.position.y > BorderTop || transform.position.y < BorderBottom) {
-						Direction.y *= -1;
+				if ((transform.position.y < BorderBottom && goingDown) || (transform.position.y > BorderTop && !goingDown)) {
+						ChangeDirectionY ();
 				}
 				transform.Translate (Direction * Time.deltaTime);
 		}
 
+		
 		void OnTriggerEnter2D (Collider2D other)
 		{
+				// Detecting collision with a player
 				if (other.gameObject == PlayerRight || other.gameObject == PlayerLeft) {
-						Direction.x *= -1;
+						ChangeDirectionX ();
 				}
 		}
 
@@ -53,7 +62,19 @@ public class BallMovement :MonoBehaviour
 		{		
 				Vector3 newPos = new Vector3 (0, 0, 0);
 				transform.position = newPos;
+				ChangeDirectionX ();
+		}
+
+		void ChangeDirectionX ()
+		{
 				Direction.x *= -1;
+				goingRight = !goingRight;
+		}
+
+		void ChangeDirectionY ()
+		{
+				Direction.y *= -1;
+				goingDown = !goingDown;
 		}
 
 }
