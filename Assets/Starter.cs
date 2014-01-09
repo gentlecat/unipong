2767cef119998092fcb70f4c8baaf7975e846994
Roman Tsukanov <roman@tsukanov.me>
2014-01-09 14:14:29 +1000

    Added title screen. Changed font.

---
+++ Assets/Starter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Starter : MonoBehaviour
+{
+
+		public string GameLevelName;
+
+		void Update ()
+		{		
+				if (Input.GetKeyDown ("space")) {
+						Application.LoadLevel (GameLevelName);
+				}
+		}
+
+}
